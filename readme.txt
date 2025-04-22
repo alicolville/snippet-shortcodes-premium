782445a64dfc57a79bd4960594d6332895b3d468
Ali Colville <email@yeken.uk>
2025-04-22 15:58:36 +0100

    Created initial files.

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Snippet Shortcodes - Premium ===
+Contributors: aliakro
+Tags: custom,shortcode,snippet,variable,library
+Requires at least: 6.0
+Tested up to: 6.8
+Stable tag: 1.0
+Requires PHP: 7.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://www.paypal.me/yeken
+
+The Premium version of Snippet Shortcodes. This plugin is required for the Premium version to work.
+
+== Description ==
+
+This plugin is required to enjoy the Premium features of Snippet Shortcodes.
+
+== Getting support ==
+
+If you have a question or an issue, please ask on the plugin's [WordPress support page](https://wordpress.org/support/plugin/shortcode-variables/), and we'll be more than happy to help.
+
+== Useful links ==
+
+* [Technical Documentation](https://snippet-shortcodes.yeken.uk/) – Installation and detailed documentation on how to use the plugin.
+* [Trial license](https://shop.yeken.uk/get-a-trial-license/) – Get a trial license to try out all the features.
+* [Upgrade](https://shop.yeken.uk/product/shortcode-variables/) – purchase a license to receive all of the features.
+
+== Upgrade Notice ==
+
+1.0 - All Snippet Shortcodes Premium features have now been moved into this plugin.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release with all Snippet Shortcodes Premium features moved from the main plugin into this one.
---
+++ snippet-shortcodes-premium.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('ABSPATH') or die("Jog on!");
+
+/**
+ * Plugin Name: Snippet Shortcodes - Premium Version
+ * Description: The Premium version of Snippet Shortcodes. This plugin is required for the Premium version to work.
+ * Version: 1.0
+ * Requires at least:   6.0
+ * Tested up to: 		6.8
+ * Requires PHP:        7.4
+ * Author:              Ali Colville
+ * Author URI:          https://www.YeKen.uk
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         snippet-shortcodes-premium
+ */
+
+/*  Copyright 2025 YeKen.uk
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define( 'YK_SS_ABSPATH', plugin_dir_path( __FILE__ ) );
+
+define( 'YK_SS_PLUGIN_VERSION', '1.0' );
+define( 'YK_SS_PLUGIN_NAME', 'Snippet Shortcodes - Premium' );
+define( 'YK_SS_SLUG', 'snippet-shortcodes-premium' );

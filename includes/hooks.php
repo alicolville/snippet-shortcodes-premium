5ac0490be6c9fe17aeede69442848858b86d1402
Ali Colville <email@yeken.uk>
2025-04-24 12:19:51 +0100

    Added version shortcode

--- includes/hooks.php
+++ includes/hooks.php
@@ -21,3 +21,15 @@ function yk_ss_menu_add_license_page() {
 	add_submenu_page( 'sh-cd-shortcode-variables-main-menu', $text, $text, 'manage_options', 'sh-cd-shortcode-variables-upgrade', 'sh_cd_advertise_pro');
 }
 add_action( 'sh-cd-admin-menu-upgrade', 'yk_ss_menu_add_license_page');	
+
+/**
+ * Render plugin version
+ *
+ * @return string
+ */
+function yk_ss_shortcode_version() {
+
+	return YK_SS_PLUGIN_VERSION;
+
+}
+add_shortcode( 'sv-premium-version', 'yk_ss_shortcode_version');

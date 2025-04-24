b1cde643919c3bc408322155458238d293a52a26
Ali Colville <email@yeken.uk>
2025-04-24 08:55:27 +0100

    Corrected function names

--- includes/cron.php
+++ includes/cron.php
@@ -5,9 +5,9 @@ defined('ABSPATH') or die("Jog on!");
 // Fetch the existing license from WP Options and run it through validation again.
 function yk_ss_cron_licence_check() {
 ;
-	$existing_license = sh_cd_license();
+	$existing_license = yk_ss_license();
 
-	sh_cd_license_apply( $existing_license );
+	yk_ss_license_apply( $existing_license );
 }
 add_action( 'daily', 'yk_ss_cron_licence_check' );
 add_action( 'sh-cd-upgrade', 'yk_ss_cron_licence_check' );
